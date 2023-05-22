37a50e099b30c3cddd7a7cf307960555e69bd21e
zhim57 <65269162+zhim57@users.noreply.github.com>
2023-05-22 19:22:46 -0400

    tide1 and tide2

--- routes/ships.js
+++ routes/ships.js
@@ -69,6 +69,41 @@ router.get("/tide1", (req, res) => {
       res.redirect("/");
     });
 });
+router.get("/tide1", (req, res) => {
+  let dateJa = new Date();
+  let dateJa1 = new Date();
+
+  dateJa1.setDate(dateJa1.getDate() + 14);
+  var dd = String(dateJa1.getDate()).padStart(2, "0");
+  var mm = String(dateJa1.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = dateJa1.getFullYear();
+  dateJa1 = mm + "/" + dd + "/" + yyyy;
+
+  var dd = String(dateJa.getDate()).padStart(2, "0");
+  var mm = String(dateJa.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = dateJa.getFullYear();
+
+  dateJa = mm + "/" + dd + "/" + yyyy;
+
+  console.log(dateJa);
+  console.log(dateJa1);
+
+  let searchQuery = {
+    date: { $gte: dateJa, $lte: dateJa1 },
+  };
+
+  console.log(searchQuery);
+  AnchSail40up.find(searchQuery) //{"date": {$slice:14}
+    .then((tides1) => {
+      console.log(tides1);
+      res.render("index", { tides: tides1 });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "ERROR: " + err);
+
+      res.redirect("/");
+    });
+});
 
 router.get("/edit/:id", (req, res) => {
   let searchQuery = { _id: req.params.id };
@@ -85,10 +120,29 @@ router.get("/edit/:id", (req, res) => {
 //get routes ends here
 
 //post routes starts here
-router.post("/api/filUpTheDateBase/", (req, res) => {
-  // console.log("api/filUpTheDateBase/")
-  // console.log(req.body);
-  let newAnchArr40up = {
+router.post("/api/filUpTheDateBase2/", (req, res) => {
+   let newAnchArr40up = {
+    date: req.body.date,
+    day: req.body.day,
+    hwsh: req.body.hwsh,
+    pred_ft: req.body.pred_ft,
+    windowOpen: req.body.windowOpen,
+    windowClose: req.body.windowClose,
+  };
+
+  AnchArr40up.create(newAnchArr40up)
+    .then((anchArr40up) => {
+      // req.flash('success_msg', 'navigational data added to database successfully.')
+      // res.redirect('/');
+    //   console.log("added to DB");
+    })
+    .catch((err) => {
+      // req.flash('error_msg', 'ERROR: '+err)
+      // res.redirect('/');
+    });
+});
+router.post("/api/filUpTheDateBase3/", (req, res) => {
+   let newAnchArr40up = {
     date: req.body.date,
     day: req.body.day,
     hwsh: req.body.hwsh,
